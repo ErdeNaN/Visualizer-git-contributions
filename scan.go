b4c683898e4d2746e0199957c8f8997ef48b3fc0
ErdeNaN <Melkor.93@mail.ru>
2020-10-02 00:41:25 +0300

    added getting filepath

--- scan.go
+++ scan.go
@@ -10,8 +10,67 @@ import(
 // scan a new folder for Git repositories
 func scan(folder string) {
     fmt.Printf("Found folders:\n\n")
-    repositories := recursiveScanFolder(folder)
+	
+	repositories := recursiveScanFolder(folder)
     filePath := getDotFilePath()
-    addNewSliceElementsToFile(filePath, repositories)
+	
+	addNewSliceElementsToFile(filePath, repositories)
     fmt.Printf("\n\nSuccessfully added\n\n")
+}
+
+// getDotFilePath returns the dot file for the repos list.
+// Creates it and the enclosing folder if it does not exist.
+func getDotFilePath() string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dotFile := usr.HomeDir + "/.gogitlocalstats"
+
+	return dotFile
+}
+
+// recursiveScanFolder starts the recursive search of git repositories
+// living in the folder subtree
+func recursiveScanFolder(folder string) []string {
+	return scanGitFolders(make([]string, 0), folder)
+}
+
+// scanGitFolders returns a list of subfolders of `folder` ending with `.git`.
+// Returns the base folder of the repo, the .git folder parent.
+// Recursively searches in the subfolders by passing an existing `folders` slice.
+func scanGitFolders(folders []string, folder string) []string {
+	// trim the last `/`
+	folder = strings.TrimSuffix(folder, "/")
+
+	f, err := os.Open(folder)
+	if err != nil {
+		log.Fatal(err)
+	}
+	files, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var path string
+
+	for _, file := range files {
+		if file.IsDir() {
+			path = folder + "/" + file.Name()
+			if file.Name() == ".git" {
+				path = strings.TrimSuffix(path, "/.git")
+				fmt.Println(path)
+				folders = append(folders, path)
+				continue
+			}
+			if file.Name() == "vendor" || file.Name() == "node_modules" {
+				continue
+			}
+			folders = scanGitFolders(folders, path)
+		}
+	}
+
+	return folders
 }
